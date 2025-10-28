db37010970e6dcac3b2ba52675de31049426e7ed
faysalmohammed2020 <faysalmohammed.shah@gmail.com>
2025-10-28 19:17:53 +0600

    kitabghor layout page also updated

--- app/kitabghor/layout.tsx
+++ app/kitabghor/layout.tsx
@@ -3,10 +3,7 @@ import { ThemeProvider } from "@/components/ecommarce/theme-provider";
 const KitabGhorLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider>
-      
-
-      <main className="mt-12 overflow-y-auto p-2 lg:p-6">{children}</main>
-      
+      <main className="overflow-y-auto p-2 lg:p-6">{children}</main>
     </ThemeProvider>
   );
 };
